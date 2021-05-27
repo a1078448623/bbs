8b2fb795780578cfd2397ff9d24c4a3bfda1d6c8
wsn <1078448623@qq.com>
2021-05-27 18:24:55 +0800

    把项目修改了以下，用SpringBoot整合了之前的东西，具体逻辑没有变，只是删除了各种繁琐的配置文件，启动方式从主函数启动，目前还没有什么问题，有问题再改一下。

---
+++ demoWeb/src/main/java/com/example/demoWeb/DemoWebApplication.java
@@ -0,0 +1,17 @@
+package com.example.demoWeb;
+/*
+ *project:JavaCode
+ *file:DemoWebApplication
+ *@author:wsn
+ **date:2021/5/27 18:12
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebApplication.class,args);
+    }
+}
